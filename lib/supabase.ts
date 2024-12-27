5a87d5f13269c7bc82e198291bbc5a959332b281
Raja Singh <rajasgh18@gmail.com>
2024-12-27 14:20:39 +0530

    feat: public env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,24 +1,25 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log(supabaseUrl)
+console.log(supabaseUrl);
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('Missing Supabase environment variables')
-  throw new Error('Missing Supabase environment variables')
+  console.error("Missing Supabase environment variables");
+  throw new Error("Missing Supabase environment variables");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // Test the connection
-supabase.from('registrations').select('*', { count: 'exact', head: true })
+supabase
+  .from("registrations")
+  .select("*", { count: "exact", head: true })
   .then(({ error }) => {
     if (error) {
-      console.error('Error connecting to Supabase:', error)
+      console.error("Error connecting to Supabase:", error);
     } else {
-      console.log('Successfully connected to Supabase')
+      console.log("Successfully connected to Supabase");
     }
-  })
-
+  });
